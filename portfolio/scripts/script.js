d0f2dec981b94f4856583afc87f9195530be941c
ritchie270 <ritchie.jenkins@gmail.com>
2023-11-27 02:37:50 +0000

    all

---
+++ portfolio/scripts/script.js
@@ -0,0 +1,14 @@
+/*
+Student Name: Ritchie Jenkins
+File Name: script.js
+Date: 11/26/2023
+*/
+
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+        } else {
+            menu.style.display = "block";
+        }
+}
